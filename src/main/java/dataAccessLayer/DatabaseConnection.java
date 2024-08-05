0b2d2b65653c4f181c1ad57ebfd5e67c49973f28
Cheng0124 <159956557+Cheng0124@users.noreply.github.com>
2024-08-05 13:20:51 -0400

    Cheng's 1st Commit

---
+++ src/main/java/dataAccessLayer/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package dataAccessLayer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private Connection connection = null;
+    private String url = "jdbc:mysql://localhost:3306/fwrp?useSSL=false&allowPublicKeyRetrieval=true";
+    private String username = "root";
+    private String password = "Algonquin";
+
+    public DatabaseConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            // Handle the exception as needed
+        }
+    }
+
+    /*
+     * Only use one connection for this application, prevent memory leaks.
+     */
+    public Connection createConnection() throws SQLException {
+        try {
+            if (connection != null) {
+                System.out.println("Cannot create new connection, one exists already");
+            } else {
+                connection = DriverManager.getConnection(url, username, password);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+        return connection;
+    }
+}
